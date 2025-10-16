21a18d37ad7af247b87fe6fa91fe21bfc6c3216d
coolport <aidanalcayde@gmail.com>
2025-10-16 10:45:27 +0800

    refactor: OOP

--- dbus.py
+++ dbus.py
@@ -2,66 +2,60 @@ import asyncio
 from dbus_next.aio import MessageBus
 
 # loop = asyncio.get_event_loop()
-
 # busctl --user list | grep mpris
 # org.mpris.MediaPlayer2.playerctld                                                           2317 playerctld      aidan :1.1          session-c1.scope c1      -
 # org.mpris.MediaPlayer2.spotify                                                           2300185 spotify         aidan :1.85211      session-c1.scope c1      -
-
 # peek
 # busctl --user introspect org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2
 
+# TODO: exception handling, typing etc
+
+# Establish connection to interfaces
+
+
+BUS_NAME = "org.mpris.MediaPlayer2.spotify"
+OBJECT_PATH = "/org/mpris/MediaPlayer2"
+PLAYER_PATH = "org.mpris.MediaPlayer2.Player"
+PROPERTY_PATH = "org.freedesktop.DBus.Properties"
+
+
+class MPrisPlayer:
+    def __init__(self, bus_name, object_path):
+        self.bus_name = bus_name
+        self.object_path = object_path
+        self.player = None
+        self.bus = None
+        self.introspection = None
+        self.obj = None
+        self.properties = None
+
+    async def connect(self):
+        self.bus = await MessageBus().connect()
+        self.introspection = await self.bus.introspect(self.bus_name, self.object_path)
+        self.obj = self.bus.get_proxy_object(
+            self.bus_name, self.object_path, self.introspection
+        )
+        self.player = self.obj.get_interface(PLAYER_PATH)
+        self.properties = self.obj.get_interface(PROPERTY_PATH)
+        print("ProxyInterface: ", type(self.player))
+
+        return self
+
+    # async def get_metadata(self):
+    #     self.propret
+    async def play_pause(self):
+        await self.player.call_play_pause()
+
 
 async def main():
-    bus = await MessageBus().connect()
-    # the introspection xml would normally be included in your project, but
-    # this is convenient for development
-    bus_name = "org.mpris.MediaPlayer2.spotify"
-    object_path = "/org/mpris/MediaPlayer2"
-
-    introspection = await bus.introspect(bus_name, object_path)
-    # print(introspection)
-    # <dbus_next.introspection.Node object at 0x7f664196b2d0>
-
-    obj = bus.get_proxy_object(
-        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2", introspection
-    )
-
-    player = obj.get_interface("org.mpris.MediaPlayer2.Player")
-    properties = obj.get_interface("org.freedesktop.DBus.Properties")
-
-    # call methods on the interface (this causes the media player to play)
-    # await player.call_play()
-    # await player.call_play_pause()
-
-    metadata = await player.get_metadata()
-    # print(metadata)
-
-    # artist = metadata["xesam:artist"].value
-    # Safer to use get with fallback
-    artist_meta = metadata.get("xesam:artist", None)
-    title_meta = metadata.get("xesam:title", None)
-    album_meta = metadata.get("xesam:album", None)
-
-    if artist_meta:
-        print(artist_meta.value)
-        # ['Esthero']
-        print("Propreties: ", properties)
-        # Propreties:  <dbus_next.aio.proxy_object.ProxyInterface object at 0x7fe7af373f50>
-
-    if title_meta:
-        print(title_meta.value)
-
-    if album_meta:
-        print(album_meta.value)
-
-    async def set_volume(x):
-        await player.set_volume(x)
-        print("New Volume", x)
-
-    volume = await player.get_volume()
-    print("Initial Volume", volume)
-    await set_volume(0.5)
+    player = MPrisPlayer(BUS_NAME, OBJECT_PATH)
+    await player.connect()
+    await player.play_pause()
 
 
 # loop.run_until_complete(main())
-asyncio.run(main())
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exiting")
