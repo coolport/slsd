6480c4c171db9a340498edc552def209fd4902f2
coolport <aidanalcayde@gmail.com>
2025-10-17 18:02:20 +0800

    refactor: OOP

--- lastfm.py
+++ lastfm.py
@@ -1,39 +1,48 @@
 import pylast
-import time
-import asyncio
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("LASTFM_API_KEY")
+API_SECRET = os.getenv("LASTFM_API_SECRET")
+USERNAME = os.getenv("LASTFM_USERNAME")
+PASSWORD = os.getenv("LASTFM_PASSWORD")
+PASSWORD_HASH = pylast.md5(PASSWORD)
+
+
+class Scrobbler:
+    def __init__(self, api_key, api_secret, username, password_hash):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.username = username
+        self.password_hash = password_hash
+        self.network = None
+
+    def connect(self):
+        self.network = pylast.LastFMNetwork(
+            api_key=self.api_key,
+            api_secret=self.api_secret,
+            username=self.username,
+            password_hash=self.password_hash,
+        )
+        return self
+
 
 def main():
-    load_dotenv()
-    print("PyLast Test")
-    API_KEY = os.getenv("LASTFM_API_KEY")
-    API_SECRET = os.getenv("LASTFM_API_SECRET")
+    scrobbler = Scrobbler(API_KEY, API_SECRET, USERNAME, PASSWORD_HASH).connect()
 
-    username = os.getenv("LASTFM_USERNAME")
-    password = os.getenv("LASTFM_PASSWORD")
-    password_hash = pylast.md5(password)
-
-    network = pylast.LastFMNetwork(
-        api_key=API_KEY,
-        api_secret=API_SECRET,
-        username=username,
-        password_hash=password_hash,
-    )
-
-    track = network.get_track("Iron Maiden", "The Nomad")
+    track = scrobbler.network.get_track("Iron Maiden", "The Nomad")
     track.love()
 
-    artist = "Iron Maiden"
-    title = "The Nomad"
-    base_timestamp = int(time.time())
-    print(base_timestamp)
-    # print(pylast.__file__)
-
-    for x in range(1):
-        ts = base_timestamp - x
-        network.scrobble(artist=artist, title=title, timestamp=ts)
+    # artist = "Iron Maiden"
+    # title = "The Nomad"
+    # base_timestamp = int(time.time())
+    # print(base_timestamp)
+    # # print(pylast.__file__)
+    #
+    # for x in range(1):
+    #     ts = base_timestamp - x
+    #     network.scrobble(artist=artist, title=title, timestamp=ts)
 
 
 if __name__ == "__main__":
