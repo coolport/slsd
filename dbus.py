2b217d8d801d77eba52b78e081bcdcd4f2cd4491
coolport <aidanalcayde@gmail.com>
2025-10-19 23:25:09 +0800

    refactor: handle main stuff within class, handle add/delete of players in []

--- dbus.py
+++ dbus.py
@@ -13,13 +13,11 @@ from dbus_next.aio import MessageBus
 #          ├── method: ListNames()
 #          └── method: NameHasOwner()
 
-# TODO: exception handling, typing etc
-
 DBUS_SERVICE_NAME = "org.freedesktop.DBus"
 DBUS_OBJECT_PATH = "/org/freedesktop/DBus"
 
-# PLAYER_SERVICE_NAME = "org.mpris.MediaPlayer2.spotify"
-PLAYER_SERVICE_NAME = "org.mpris.MediaPlayer2.audacious"
+PLAYER_SERVICE_NAME = "org.mpris.MediaPlayer2.spotify"
+# PLAYER_SERVICE_NAME = "org.mpris.MediaPlayer2.audacious"
 MP2_OBJECT_PATH = "/org/mpris/MediaPlayer2"
 PLAYER_INTERFACE_NAME = "org.mpris.MediaPlayer2.Player"
 
@@ -27,8 +25,8 @@ PROPERTY_NAME = "org.freedesktop.DBus.Properties"
 
 
 class ServiceManager:
-    def __init__(self, dbus_service_name, dbus_object_path, callback=None):
-        self.players = {}
+    def __init__(self, dbus_service_name, dbus_object_path):
+        self.players = []
         self.service_name = dbus_service_name
         self.object_path = dbus_object_path
         self.bus = None
@@ -36,19 +34,35 @@ class ServiceManager:
         self.introspection = None
         self.properties = None
         self.interface = None
-        self.callback = callback
 
     def owner_change_callback(self, name, old_owner, new_owner):
-        if self.callback is not None:
-            self.callback(name, old_owner, new_owner)
+        if name.startswith("org.mpris.MediaPlayer2."):
+            print(f"\nPlayer change: {name}, Old: {old_owner}, New: {new_owner}")
+            if new_owner and not old_owner:
+                print(f"\nPlayer {name} found, adding to players[]")
+                self.players.append(name)
+                print(self.players)
+            elif old_owner and not new_owner:
+                print(f"\n{name} was closed. Removing from players[]")
+                self.players.remove(name)
+                print(self.players)
+        return self
 
     async def connect(self):
-        self.bus = await MessageBus().connect()
+        try:
+            self.bus = await MessageBus().connect()
+        except e:
+            print("Failed: ", e)
+
+        print("DBus Connection Succesful")
         self.introspection = await self.bus.introspect(
-            self.service_name, self.object_path
+            self.service_name,
+            self.object_path,
         )
         self.object = self.bus.get_proxy_object(
-            self.service_name, self.object_path, self.introspection
+            self.service_name,
+            self.object_path,
+            self.introspection,
         )
         self.interface = self.object.get_interface(self.service_name)
 
@@ -57,22 +71,20 @@ class ServiceManager:
         names = await self.interface.call_list_names()
         for x in names:
             if x.startswith("org.mpris.MediaPlayer2."):
-                print(x)
+                self.players.append(x)
+        print("Found players: ", self.players)
 
         return self
 
 
 class MPrisPlayer:
     def __init__(
-        self,
-        service_name,
-        object_path,
-        change_callback_function=None,
+        self, service_name, object_path, change_callback_function=None, bus=None
     ):
         self.service_name = service_name
         self.object_path = object_path
         self.player = None
-        self.bus = None
+        self.bus = bus
         self.introspection = None
         self.object = None
         self.properties = None
@@ -93,23 +105,25 @@ class MPrisPlayer:
                 await self.change_callback_function(artist, track)
 
     async def connect(self):
-        self.bus = await MessageBus().connect()
+        bus = self.bus
         self.introspection = await self.bus.introspect(
-            self.service_name, self.object_path
+            self.service_name,
+            self.object_path,
         )
         self.object = self.bus.get_proxy_object(
-            self.service_name, self.object_path, self.introspection
+            self.service_name,
+            self.object_path,
+            self.introspection,
         )
         self.player = self.object.get_interface(PLAYER_INTERFACE_NAME)
         self.properties = self.object.get_interface(PROPERTY_NAME)
-        print("DBus Connection Success\nProxyInterface: ", type(self.player))
 
         self.properties.on_properties_changed(self.signal_change_callback)
 
         return self
 
-    async def get_metadata(self):
-        metadata = await self.player.get_metadata()
-        self.metadata = metadata
-
-        return metadata
+    # async def get_metadata(self):
+    #     metadata = await self.player.get_metadata()
+    #     self.metadata = metadata
+    #
+    #     return metadata
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from dbus import MPrisPlayer, ServiceManager
 from lastfm import Scrobbler
 
 load_dotenv()
-# SERVICE_NAME = "org.mpris.MediaPlayer2.spotify"
-SERVICE_NAME = "org.mpris.MediaPlayer2.audacious"
+SERVICE_NAME = "org.mpris.MediaPlayer2.spotify"
+# SERVICE_NAME = "org.mpris.MediaPlayer2.audacious"
 OBJECT_PATH = "/org/mpris/MediaPlayer2"
 API_KEY = os.getenv("LASTFM_API_KEY")
 API_SECRET = os.getenv("LASTFM_API_SECRET")
@@ -19,13 +19,15 @@ PASSWORD_HASH = pylast.md5(PASSWORD)
 DBUS_SERVICE_NAME = "org.freedesktop.DBus"
 DBUS_OBJECT_PATH = "/org/freedesktop/DBus"
 
-# class ScrobbleQueue:
-#     def __init__(self):
-
 
 async def catch_change(artist, track):
     print(f"\nTracked Changed To:\n{track} - {artist}")
-    scrobbler = Scrobbler(API_KEY, API_SECRET, USERNAME, PASSWORD_HASH)
+    scrobbler = Scrobbler(
+        API_KEY,
+        API_SECRET,
+        USERNAME,
+        PASSWORD_HASH,
+    )
 
     await asyncio.to_thread(scrobbler.connect)
     # Delay .scrobble call instead of invoking the function and passing that value
@@ -35,20 +37,16 @@ async def catch_change(artist, track):
     return artist, track
 
 
-def catch_owner_change(name, old_owner, new_owner):
-    print(f"Player change: {name}, Old: {old_owner}, New: {new_owner}")
-
-
 async def main():
-    player = MPrisPlayer(SERVICE_NAME, OBJECT_PATH, catch_change)
-    service_manager = ServiceManager(
-        DBUS_SERVICE_NAME, DBUS_OBJECT_PATH, catch_owner_change
+    service_manager = ServiceManager(DBUS_SERVICE_NAME, DBUS_OBJECT_PATH)
+    await service_manager.connect()
+    player = MPrisPlayer(
+        SERVICE_NAME,
+        OBJECT_PATH,
+        catch_change,
+        service_manager.bus,
     )
     await player.connect()
-    await service_manager.connect()
-
-    meta = await player.get_metadata()
-    print("\nPlayer Metadata: ", meta)
 
     try:
         while True:
