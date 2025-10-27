ffa9120887b3ffb4f400a28f2239948f7d39db3b
coolport <aidanalcayde@gmail.com>
2025-10-28 00:39:36 +0800

    feat: parse and use config

--- src/slsd/main.py
+++ src/slsd/main.py
@@ -1,29 +1,42 @@
 import asyncio
 import hashlib
+import tomllib
 import os
-from dotenv import load_dotenv
+from pathlib import Path
 from dbus import ServiceManager
 from lastfm import Scrobbler
-
-load_dotenv()
-API_KEY = os.getenv("LASTFM_API_KEY")
-API_SECRET = os.getenv("LASTFM_API_SECRET")
-USERNAME = os.getenv("LASTFM_USERNAME")
-PASSWORD = os.getenv("LASTFM_PASSWORD")
-PASSWORD_HASH = hashlib.md5(PASSWORD.encode("utf-8")).hexdigest()
 
 DBUS_SERVICE_NAME = "org.freedesktop.DBus"
 DBUS_OBJECT_PATH = "/org/freedesktop/DBus"
 
+CONFIG_HOME = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
+CONFIG_FILE = CONFIG_HOME / "slsd" / "config.toml"
+
+try:
+    with open(CONFIG_FILE, "rb") as config:
+        config_data = tomllib.load(config)
+    if config_data:
+        USERNAME = config_data.get("credentials").get("username")
+        PASSWORD = config_data.get("credentials").get("password")
+        API_KEY = config_data.get("credentials").get("api_key")
+        API_SECRET = config_data.get("credentials").get("api_secret")
+        PASSWORD_HASH = hashlib.md5(PASSWORD.encode("utf-8")).hexdigest()
+        BLACKLIST = config_data.get("options").get("blacklist")
+except Exception as e:
+    print("Failed: ", e)
+
 
 async def catch_property_change(artist, track):
     print(f"\nTracked Changed To:\n{track} - {artist}")
-    scrobbler = Scrobbler(
-        API_KEY,
-        API_SECRET,
-        USERNAME,
-        PASSWORD_HASH,
-    )
+    try:
+        scrobbler = Scrobbler(
+            API_KEY,
+            API_SECRET,
+            USERNAME,
+            PASSWORD_HASH,
+        )
+    except Exception as e:
+        print("Failed to connect: ", e)
 
     await asyncio.to_thread(scrobbler.connect)
     # Delay .scrobble call instead of invoking the function and passing that value
@@ -39,6 +52,7 @@ async def main():
         DBUS_SERVICE_NAME,
         DBUS_OBJECT_PATH,
         catch_property_change,
+        BLACKLIST,
     )
     await service_manager.connect()
 
